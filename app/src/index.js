2b36db442fd6cbf5137395be6dbf8da81dbfac85
Vadym Chutur <falloutgreck@gmail.com>
2024-09-24 16:21:43 +0300

    upd

---
+++ app/src/index.js
@@ -0,0 +1,2 @@
+import './css/common.scss';
+import './js/script.js';
---
+++ app/webpack.config.js
@@ -0,0 +1,38 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const LiveReloadPlugin = require('webpack-livereload-plugin');
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'main.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.scss$/i,
+        use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],
+      },
+      {
+        test: /\.m?js$/,
+        exclude: /node_modules/,
+        use: ['babel-loader'],
+      },
+    ],
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({ template: './src/index.html' }),
+    new MiniCssExtractPlugin({ filename: 'styles.css' }),
+    new LiveReloadPlugin(),
+  ],
+  devServer: {
+    port: 5050,
+    open: true,
+    liveReload: true,
+  },
+  // stats: 'errors-only',
+};
