9cf5d0ad1613945dcd2637017486978e940fc668
Vadym Chutur <falloutgreck@gmail.com>
2024-09-25 17:03:05 +0300

    upd

--- app/src/js/script.js
+++ app/src/js/script.js
@@ -1,1 +1,18 @@
 console.log('Hello World');
+
+const containerEl = document.querySelector('.js-container');
+
+// [...containerEl.children].forEach((item) =>
+//   item.addEventListener('click', onClick)
+// );
+
+containerEl.addEventListener('click', onClick);
+
+function onClick(event) {
+  if (event.target.classList.contains('js-box')) {
+    console.log(event.currentTarget);
+    console.log(event.target);
+  } else {
+    console.log('error');
+  }
+}
